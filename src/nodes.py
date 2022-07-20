8bb0e443f55d3202091a6da04398c4582a654323
pepepapi <samuel.iga03@gmail.com>
2022-07-20 22:43:56 +0100

    added node idea

---
+++ src/nodes.py
@@ -0,0 +1,16 @@
+from copyreg import constructor
+
+
+from logicope import *
+
+#this is just an idea. it doesn't work at
+class Nodes:
+    def __init__(self, type, state, wireTo):
+        self.type = type
+        self.state = state
+        self.wireTo = wireTo
+
+    def _log(self):
+        pass
+    
+print(Nodes(LogiCope._and(True, True), False, False))
